25f777e651c34cf105ec5353655bec6113846a4f
YllanSUTTER <sutter.yllan@laposte.net>
2024-04-20 10:48:49 +0200

    Next Todo / first share

---
+++ components/shared/DeleteTodo.tsx
@@ -0,0 +1,26 @@
+"use client"
+import { deleteTodoLine } from "@/app/actions/todoActions";
+import Button from "../ui/Button";
+import Form from "../ui/Form";
+import Input from "../ui/Input";
+import { todoType } from "@/types/todoType";
+import { BsFillTrashFill } from "react-icons/bs";
+
+const deleteTodo = ({todo}: {todo:todoType}) => {
+  return (
+   <Form action={deleteTodoLine}>
+    <Input
+      type="hidden"
+      name="inputId"
+      value={todo.id}
+    />
+    <Button
+      actionButton
+      text={<BsFillTrashFill/>}
+      type="submit"
+    />
+   </Form>
+  )
+}
+
+export default deleteTodo
---
+++ components/ui/Form.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import { useRef,ReactNode } from "react"
+interface formProps {
+  children : ReactNode;
+  action : (formData: FormData) => Promise<void |boolean>;
+  className?: string;
+  onSubmit?: () => void;
+}
+
+const Form = ({children, action, className, onSubmit}:formProps) => {
+  const ref = useRef<HTMLFormElement>(null) //permet de faire en sorte que le form se reset quand on le lance
+  return  <form 
+  className="{className}" 
+  onSubmit={onSubmit} 
+  ref={ref} 
+  action={async (formData) => {
+      await action(formData);
+      ref.current?.reset();
+    }}
+  >{children}</form>
+  
+}
+
+export default Form
